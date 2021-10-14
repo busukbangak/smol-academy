87fef03e0a4204d362b4213393f46c4914489fd3
awanali <aliim.wanali@gmail.com>
2021-10-14 21:25:15 +0200

    Add CursorManager

---
+++ Scriptscenes/Autoloads/Managers/CursorManager.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+public enum CursorType
+{
+    Default,
+    Attack
+}
+
+public class CursorManager : Node
+{
+    [Export]
+    private Resource _defaultCursorImage;
+    private static Resource _staticDefaultCursorImage;
+
+    [Export]
+    private Resource _attackCursorImage;
+    private static Resource _staticAttackCursorImage;
+
+
+    public override void _Ready()
+    {
+        _staticDefaultCursorImage = _defaultCursorImage;
+        _staticAttackCursorImage = _attackCursorImage;
+        ChangeCursor(CursorType.Default);
+    }
+
+    public static void ChangeCursor(CursorType cursorType)
+    {
+        switch (cursorType)
+        {
+            case CursorType.Default:
+                Input.SetCustomMouseCursor(_staticDefaultCursorImage);
+                break;
+            case CursorType.Attack:
+                Input.SetCustomMouseCursor(_staticAttackCursorImage);
+                break;
+        }
+    }
+
+
+}
