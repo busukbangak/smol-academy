9674d4cebe09c506e824420900b43ebb28d12fb9
awanali <aliim.wanali@gmail.com>
2021-10-17 16:10:14 +0200

    Add turrets and adjusted paths and nexus size

--- Scriptscenes/Entities/Buildings/Turret.cs
+++ Scriptscenes/Entities/Buildings/Turret.cs
@@ -3,19 +3,19 @@ using System;
 
 public class Turret : Entity
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        base._Ready();
+        ChangeTurretColor(AssignedTeam == TeamColor.Blue ? Colors.SteelBlue : Colors.OrangeRed);
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    private void ChangeTurretColor(Color color)
+    {
+        //TODO: Need to duplicate material and set it, because jsut creating the new one will replace it
+        SpatialMaterial spatialMaterial = new SpatialMaterial();
+        spatialMaterial.AlbedoColor = color;
+
+        Model.GetNode<CSGMesh>("Turret/Bot").Set("material", spatialMaterial);
+    }
 }
