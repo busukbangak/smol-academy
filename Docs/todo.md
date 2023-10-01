b0028f5826ec86672637afeaa3b7c6191433a79b
awanali <aliim.wanali@gmail.com>
2023-10-01 23:43:13 +0200

    update icon, restructured project and add todo

---
+++ Docs/todo.md
@@ -0,0 +1,13 @@
+- project structure cleanup
+- check copyright (replace cursors, replace mech)
+- camera offset limit
+- lock mouse
+- menu
+- game start, finish and reset
+- minion wave spawns
+- more abilities
+- multiplayer
+
+
+- better camera scrolling
+- navigation system updaten
---
+++ Scripts/Entities/Characters/Abilities/BlinkAbility.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public class BlinkAbility : Ability
+{
+
+    public float MaxRange = 15f;
+
+    public override void Cast()
+    {
+        var mousePosition = (Vector3)Utilities.MouseRaycast(Smol.GetViewport().GetCamera())["position"];
+        var flooredMousePosition = new Vector3(mousePosition.x, Smol.Translation.y, mousePosition.z);
+        var direction = Smol.GlobalTransform.origin.DirectionTo(flooredMousePosition);
+        var distanceToMousePosition = Smol.GlobalTransform.origin.DistanceTo(mousePosition);
+        Smol.Model.LookAt(Smol.GlobalTransform.origin + direction, Vector3.Up);
+        Smol.GlobalTranslate(direction * (distanceToMousePosition <= MaxRange ? distanceToMousePosition : MaxRange ));
+    }
+}
