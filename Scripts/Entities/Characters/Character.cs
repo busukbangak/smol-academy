3e40e9b3dd3907bfe08cd6ebd153db8417f3fc53
awanali <aliim.wanali@gmail.com>
2023-10-06 23:37:06 +0200

    Fixed Turret Collision Bug

--- Scripts/Entities/Characters/Character.cs
+++ Scripts/Entities/Characters/Character.cs
@@ -95,10 +95,20 @@ public abstract class Character : Entity
     {
         base.OnDead();
 
+        // TODO: Do it with collision masks and layers
+        GetNode<CollisionShape>("CollisionShape").Disabled = true;
+
         if (!IsRespawnActivated)
         {
             GetNode<Timer>("NavigationTimer").Disconnect("timeout", this, "OnNavigationTimerTimeout");
         }
+    }
+
+    public override void OnRespawn()
+    {
+        base.OnRespawn();
+        // TODO: Do it with collision masks and layers
+        GetNode<CollisionShape>("CollisionShape").Disabled = false;
     }
 
     public void OnNavigationTimerTimeout()
