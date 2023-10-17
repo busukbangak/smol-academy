b418a5b04cae55dd879c382ac8b90dc27b21a82c
awanali <aliim.wanali@gmail.com>
2023-10-17 21:46:59 +0200

    Quickfix hack proejctile target null

--- Scripts/Shared/Hitbox/Projectile/Projectile.cs
+++ Scripts/Shared/Hitbox/Projectile/Projectile.cs
@@ -17,7 +17,7 @@ public class Projectile : Hitbox
     {
         Target = target;
         SetAsToplevel(true);
-        _velocity = -(Transform.basis.z) * 0.2f * _speed;
+        _velocity = -Transform.basis.z * 0.2f * _speed;
     }
 
     public override void _Process(float delta)
@@ -27,22 +27,23 @@ public class Projectile : Hitbox
             return;
         }
 
-        // TODO BUGFIX: when projectile flies to minion which is already queued free
-        var targetLocation = Target?.GlobalTransform.origin;
-        var desiredVelocity = (targetLocation - Transform.origin)?.Normalized() * _speed;
-        var steer = (desiredVelocity - _velocity)?.Normalized() * _steerForce;
-
-        var velocity = steer * delta;
-        if (velocity != null)
+        // TODO BUGFIX: when projectile flies to minion which is already queued free, this is quickfix hack
+        try
         {
+            var targetLocation = Target.GlobalTransform.origin;
+            var desiredVelocity = (targetLocation - Transform.origin).Normalized() * _speed;
+            var steer = (desiredVelocity - _velocity).Normalized() * _steerForce;
+
+            var velocity = steer * delta;
             _velocity += (Vector3)velocity;
             LookAt(Transform.origin + _velocity, Vector3.Up);
             Translation += _velocity * delta;
         }
-        else
+        catch (System.Exception)
         {
             QueueFree();
         }
+
     }
 
     public override void OnHitboxAreaEntered(Area area)
